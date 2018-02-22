443eb05bbe392d2f67d7e2dbddcef5c0195b48d6
Marinus Geuze <marinus@familiegeuze.nl>
2018-02-22 21:20:10 +0100

    Delete Account example code

--- src/main/java/com/example/axonbank/query/AccountBalanceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.axonbank.query;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountBalanceRepository extends JpaRepository<AccountBalance, String> {
-}
--- src/main/java/com/example/axonbank/query/TransactionHistoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.axonbank.query;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Long> {
-}
